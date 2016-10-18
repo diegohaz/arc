609d34c9ffa8dd082b89688f0b2a12fafb765dbb
Diego Haz <hazdiego@gmail.com>
2016-10-18 11:42:53 -0200

    Update IconButton and IconLink components

--- src/components/molecules/IconButton/IconButton.js
+++ src/components/molecules/IconButton/IconButton.js
@@ -3,31 +3,41 @@ import styled, { css } from 'styled-components'
 
 import { Icon, Button } from 'components'
 
-const IconButton = styled(({ icon, right, responsive, children, ...props }) => {
-  const iconElement = <Icon size={16} icon={icon} className="icon" />
+const iconStyles = ({ hasText, right, responsive }) => css`
+  margin: ${hasText ? (right ? '0 0 0 0.5rem' : '0 0.5rem 0 0') : 0};
+  @media screen and (max-width: 420px) {
+    margin: ${responsive && 0};
+  }
+`
+
+const textStyle = ({ responsive }) => css`
+  vertical-align: middle;
+  @media screen and (max-width: 420px) {
+    display: ${responsive && 'none'};
+  }
+`
+
+const StyledIcon = styled(Icon)`${iconStyles}`
+const Text = styled.span`${textStyle}`
+
+const IconButton = ({ icon, right, responsive, children, ...props }) => {
+  const iconElement = (
+    <StyledIcon
+      size={16}
+      icon={icon}
+      hasText={!!children}
+      right={right}
+      responsive={responsive}
+    />
+  )
   return (
     <Button {...props}>
       {right || iconElement}
-      <span className="text">{children}</span>
+      <Text responsive={responsive}>{children}</Text>
       {right && iconElement}
     </Button>
   )
-})`
-${({ right, responsive, children }) => css`
-  & > .text {
-    vertical-align: middle;
-    @media screen and (max-width: 420px) {
-      display: ${responsive && 'none'};
-    }
-  }
-
-  & > .icon {
-    margin: ${children ? (right ? '0 0 0 0.5rem' : '0 0.5rem 0 0') : 0};
-    @media screen and (max-width: 420px) {
-      margin: ${responsive && 0};
-    }
-  }
-`}`
+}
 
 IconButton.propTypes = {
   icon: PropTypes.string.isRequired,
--- src/components/molecules/IconLink/IconLink.js
+++ src/components/molecules/IconLink/IconLink.js
@@ -3,31 +3,41 @@ import styled, { css } from 'styled-components'
 
 import { Icon, Link } from 'components'
 
-const IconLink = styled(({ icon, right, responsive, children, ...props }) => {
-  const iconElement = <Icon size={16} icon={icon} className="icon" />
+const iconStyles = ({ hasText, right, responsive }) => css`
+  margin: ${hasText ? (right ? '0 0 0 0.3rem' : '0 0.3rem 0 0') : 0};
+  @media screen and (max-width: 420px) {
+    margin: ${responsive && 0};
+  }
+`
+
+const textStyle = ({ responsive }) => css`
+  vertical-align: middle;
+  @media screen and (max-width: 420px) {
+    display: ${responsive && 'none'};
+  }
+`
+
+const StyledIcon = styled(Icon)`${iconStyles}`
+const Text = styled.span`${textStyle}`
+
+const IconLink = ({ icon, right, responsive, children, ...props }) => {
+  const iconElement = (
+    <StyledIcon
+      size={16}
+      icon={icon}
+      hasText={!!children}
+      right={right}
+      responsive={responsive}
+    />
+  )
   return (
     <Link {...props}>
       {right || iconElement}
-      <span className="text">{children}</span>
+      <Text responsive={responsive}>{children}</Text>
       {right && iconElement}
     </Link>
   )
-})`
-${({ right, responsive, children }) => css`
-  & > .text {
-    vertical-align: middle;
-    @media screen and (max-width: 420px) {
-      display: ${responsive && 'none'};
-    }
-  }
-
-  & > .icon {
-    margin: ${children ? (right ? '0 0 0 0.3rem' : '0 0.3rem 0 0') : 0};
-    @media screen and (max-width: 420px) {
-      margin: ${responsive && 0};
-    }
-  }
-`}`
+}
 
 IconLink.propTypes = {
   icon: PropTypes.string.isRequired,
