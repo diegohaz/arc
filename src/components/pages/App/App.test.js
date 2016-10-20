404f2aba48c5ac98fe70dabb69efcaea86496203
Diego Haz <hazdiego@gmail.com>
2016-10-20 19:37:16 -0200

    Move App from pages to the components root directory It's clearly a top level component and not a page

--- src/components/pages/App/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from './App'
-
-it('renders', () => {
-  shallow(<App />)
-})
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,2 +1,1 @@
-export App from './App/App'
 export HomePage from './HomePage/HomePage'
