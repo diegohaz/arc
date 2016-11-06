b34bc60b2a467af3eadf1b4c68883c83bb651786
Diego Haz <hazdiego@gmail.com>
2016-11-06 02:42:18 -0200

    Add NotFoundPage

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import { App, HomePage, SamplePage } from 'components'
+import { App, HomePage, SamplePage, NotFoundPage } from 'components'
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="/sample-page" component={SamplePage} />
+    <Route path="*" component={NotFoundPage} />
   </Route>
 )
 
