5564771e7ee3f268a41b8e63d7f6139688548842
Diego Haz <hazdiego@gmail.com>
2016-11-22 15:10:27 -0200

    Add icon metatag on server side

--- src/components/Html.js
+++ src/components/Html.js
@@ -11,6 +11,7 @@ const Html = ({ styles, assets, state, content }) => {
         {helmet.title.toComponent()}
         {helmet.meta.toComponent()}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {helmet.link.toComponent()}
         <style dangerouslySetInnerHTML={{ __html: styles }} />
       </head>
       <body>
