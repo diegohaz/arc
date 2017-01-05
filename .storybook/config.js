ac313664a36510eb02f6e41e8d844bf36094e722
Diego Haz <hazdiego@gmail.com>
2017-01-05 04:14:07 -0200

    Add theme to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import { configure } from '@kadira/storybook'
+import React from 'react'
+import { configure, addDecorator } from '@kadira/storybook'
+import { ThemeProvider } from 'styled-components'
 
-const req = require.context('../src/components', true, /.stories.js$/)
+import theme from 'components/themes/default'
+
+const req = require.context('components', true, /.stories.js$/)
 
 function loadStories () {
   req.keys().forEach((filename) => req(filename))
 }
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+))
 
 configure(loadStories, module)
