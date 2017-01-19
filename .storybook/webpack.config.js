f4e2301371291e0da6b40a64fc22da92bb5e7e4e
Diego Haz <hazdiego@gmail.com>
2017-01-19 07:36:56 -0200

    Add webpack loaders to storybook webpack config fix #58

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,5 +2,6 @@ var baseConfig = require('../webpack.config')
 
 module.exports = function (storybookBaseConfig, configType) {
   storybookBaseConfig.resolve = baseConfig.resolve
+  storybookBaseConfig.module.loaders.push(...baseConfig.module.loaders)
   return storybookBaseConfig
 }
