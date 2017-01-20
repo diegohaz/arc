c021b535bfd9cf904bd7f21ccc27a1bc350fb7be
Diego Haz <hazdiego@gmail.com>
2017-01-20 08:05:52 -0200

    Update storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,7 @@ const baseConfig = require('../webpack.config')
 module.exports = storybookBaseConfig =>
   Object.assign({}, storybookBaseConfig, {
     resolve: baseConfig.resolve,
-    module: Object.assign({}, storybookBaseConfig, {
+    module: Object.assign({}, storybookBaseConfig.module, {
       loaders: storybookBaseConfig.module.loaders.concat(baseConfig.module.loaders)
     })
   })
