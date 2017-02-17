a08f0bea59ef2d3ea861bf7cddf60adcf92c5dd2
Ruslan Kyba <kybargr@gmail.com>
2017-02-17 13:56:22 +0200

    Efficient lodash import

--- src/containers/PostForm.js
+++ src/containers/PostForm.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
-import { merge } from 'lodash'
+import merge from 'lodash/merge'
 import { fromForm, fromEntities } from 'store/selectors'
 import { postCreate, postRead, postUpdate } from 'store/actions'
 import { createValidator, required } from 'services/validation'
