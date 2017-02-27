71ae1034e7dd4224f9a4c75e941caa38f52e7fe6
Diego Haz <hazdiego@gmail.com>
2017-02-27 15:05:29 -0300

    Update the API service for more functional programming

--- src-clean/services/api/index.js
+++ src-clean/services/api/index.js
@@ -1,51 +1,95 @@
-import fetch from 'isomorphic-fetch'
+import 'whatwg-fetch'
+import { stringify } from 'query-string'
+import merge from 'lodash/merge'
 import { apiUrl } from 'config'
 
-const api = {}
-const headers = {}
-
-api.request = (endpoint, method, settings, body) => {
-  const url = (endpoint.indexOf(apiUrl) === -1) ? apiUrl + endpoint : endpoint
-
-  return fetch(url, api.configureRequest(method, settings, body))
-    .then(api.checkStatus)
-    .then(response => response)
-    .catch(error => Promise.reject(error))
-}
-
-api.configureRequest = (method = 'GET', settings = {}, body = null) => {
-  headers.Accept = 'application/json'
-  headers['Content-Type'] = 'application/json'
-  if (settings.accessToken) { headers.Authorization = `Bearer ${settings.accessToken}` }
-  if (settings.locale) { headers['Accept-Language'] = settings.locale }
-
-  const fetchInit = { method, headers }
-
-  if (body) { fetchInit.body = JSON.stringify(body) }
-
-  return fetchInit
-}
-
-api.checkStatus = (response) => {
+export const checkStatus = (response) => {
   if (response.ok) {
-    return response.json()
+    return response
   }
-
-  return response.json()
-    .then((error) => {
-      throw new Error(`${response.statusText} (${response.status}) error occurred downstream: ${error.message}`)
-    })
+  const error = new Error(`${response.status} ${response.statusText}`)
+  error.response = response
+  throw error
 }
 
-api.setToken = token => (headers.Authorization = `Bearer ${token}`)
-api.unsetToken = () => (headers.Authorization = null)
+export const parseJSON = response => response.json()
 
-;['delete', 'get', 'head'].forEach((method) => {
-  api[method] = (url, settings) => api.request(url, method, settings)
+export const parseSettings = ({ method = 'get', data, locale, ...otherSettings } = {}) => {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Accept-Language': locale,
+  }
+  const settings = {
+    body: data ? JSON.stringify(data) : undefined,
+    method,
+    headers,
+    ...otherSettings,
+  }
+  return settings
+}
+
+export const parseEndpoint = (endpoint, params) => {
+  const url = endpoint.indexOf('http') === 0 ? endpoint : apiUrl + endpoint
+  const querystring = params ? `?${stringify(params)}` : ''
+  return `${url}${querystring}`
+}
+
+const api = {}
+
+api.request = (endpoint, { params, ...settings } = {}) =>
+  fetch(parseEndpoint(endpoint, params), parseSettings(settings))
+    .then(checkStatus)
+    .then(parseJSON)
+
+;['delete', 'get'].forEach((method) => {
+  api[method] = (endpoint, settings) => api.request(endpoint, { method, ...settings })
 })
 
 ;['post', 'put', 'patch'].forEach((method) => {
-  api[method] = (url, data, settings) => api.request(url, method, settings, data)
+  api[method] = (endpoint, data, settings) => api.request(endpoint, { method, data, ...settings })
+})
+
+api.create = (settings = {}) => ({
+  settings,
+
+  setToken(token) {
+    this.settings.headers = {
+      ...this.settings.headers,
+      Authorization: `Bearer ${token}`,
+    }
+  },
+
+  unsetToken() {
+    this.settings.headers = {
+      ...this.settings.headers,
+      Authorization: undefined,
+    }
+  },
+
+  request(endpoint, settings) {
+    return api.request(endpoint, merge({}, this.settings, settings))
+  },
+
+  post(endpoint, data, settings) {
+    return this.request(endpoint, { method: 'post', data, ...settings })
+  },
+
+  get(endpoint, settings) {
+    return this.request(endpoint, { method: 'get', ...settings })
+  },
+
+  put(endpoint, data, settings) {
+    return this.request(endpoint, { method: 'put', data, ...settings })
+  },
+
+  patch(endpoint, data, settings) {
+    return this.request(endpoint, { method: 'patch', data, ...settings })
+  },
+
+  delete(endpoint, settings) {
+    return this.request(endpoint, { method: 'delete', ...settings })
+  },
 })
 
 export default api
