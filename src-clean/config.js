80b3b006969fb322e5bc80a94c242814f70989ad
Diego Haz <hazdiego@gmail.com>
2017-02-27 15:09:07 -0300

    Update config.js

--- src-clean/config.js
+++ src-clean/config.js
@@ -1,14 +1,9 @@
 const merge = require('lodash/merge')
 
-const browser = typeof window !== 'undefined'
-
 const config = {
   all: {
     env: process.env.NODE_ENV || 'development',
     apiUrl: 'https://jsonplaceholder.typicode.com',
-    fbAppId: '991453140998882',
-    googleClientId: '464712936089-q953apdu1bjiqtcjndktnnk1ts4f2cgv.apps.googleusercontent.com',
-    browser,
   },
   test: {},
   development: {},
