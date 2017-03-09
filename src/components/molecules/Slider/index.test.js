90a0f21fcec079b64ccb82027d3ba48846361e71
Sebastian MacDonald <optissimum@gmail.com>
2017-03-09 01:01:17 -0500

    Replace Slider default tests with real ones

--- src/components/molecules/Slider/index.test.js
+++ src/components/molecules/Slider/index.test.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import Slider from '.'
 
 const wrap = (props = {}) => shallow(<Slider {...props} />)
 
-it('renders children when passed in', () => {
-  const wrapper = wrap({ children: 'test' })
-  expect(wrapper.contains('test')).toBe(true)
-})
-
-it('renders props when passed in', () => {
+it('renders input props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })
   expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
+})
+
+it('mounts with different combination of props', () => {
+  mount(<Slider disabled />)
+  mount(<Slider responsive />)
+  mount(<Slider reverse />)
 })
