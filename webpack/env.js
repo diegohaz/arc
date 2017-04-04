ea6ff9e8c42e12111e716ef918cf8b98acd773dd
Ryan Garant <ryantgarant@gmail.com>
2017-04-03 17:55:38 -0700

    [redux-ssr] changed 0.0.0.0 to localhost

--- webpack/env.js
+++ webpack/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ip: process.env.IP || '0.0.0.0',
+  ip: process.env.IP || 'localhost',
   port: (+process.env.PORT + 1) || 3001,
   isDev: process.env.NODE_ENV !== 'production',
   publicPath: `/${process.env.PUBLIC_PATH || ''}/`.replace('//', '/'),
