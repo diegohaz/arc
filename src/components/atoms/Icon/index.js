a2736f591df076ebafe57d9c297624c85ced2fbc
Ruslan Kyba <kybargr@gmail.com>
2017-04-12 19:49:35 +0300

    Updated components and packchages.

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { palette } from 'styled-theme'
 import { ifProp } from 'styled-tools'
--- src/components/molecules/Table/index.js
+++ src/components/molecules/Table/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
 
