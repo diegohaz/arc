5a602fd07e8c10ef12b995222b5ec956e7192963
Ruslan Kyba <kybargr@gmail.com>
2017-04-12 20:25:33 +0300

    Fixed lint

--- src-clean/routes.js
+++ src-clean/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { Switch } from 'react-router'
 import { renderRoutes } from 'react-router-config'
 
 import App from 'components/App'
