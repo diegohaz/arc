e0b9a6baa47ea2deb8311a899aab10a5b511b73f
Diego Haz <hazdiego@gmail.com>
2017-04-26 22:02:59 -0300

    Update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,8 @@
 // https://github.com/diegohaz/arc/wiki/Storybook
 import React from 'react'
 import { configure, addDecorator } from '@kadira/storybook'
-import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-
+import { ThemeProvider } from 'styled-components'
 import theme from 'components/themes/default'
 
 const req = require.context('components', true, /.stories.js$/)
