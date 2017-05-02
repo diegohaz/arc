5f63b7f697791fb5e5d2fb1b82cef0b675f74a57
Diego Haz <hazdiego@gmail.com>
2017-05-01 21:57:57 -0300

    Beautify code

--- src-example/services/validation/index.js
+++ src-example/services/validation/index.js
@@ -3,35 +3,35 @@ import isInt from 'validator/lib/isInt'
 import isIn from 'validator/lib/isIn'
 import isURL from 'validator/lib/isURL'
 
-const isEmpty = (value) => value === undefined || value === null || value === ''
-const join = (rules) => (value, data) =>
-  rules.map((rule) => rule(value, data)).filter((error) => !!error)[0]
+const isEmpty = value => value === undefined || value === null || value === ''
+const join = rules => (value, data) =>
+  rules.map(rule => rule(value, data)).filter(error => !!error)[0]
 
-export const email = (value) => !isEmpty(value) && !isEmail(value) &&
+export const email = value => !isEmpty(value) && !isEmail(value) &&
   'Invalid email address'
 
-export const url = (value) => !isEmpty(value) && !isURL(value) &&
+export const url = value => !isEmpty(value) && !isURL(value) &&
   'Invalid URL'
 
-export const required = (value) => isEmpty(value) &&
+export const required = value => isEmpty(value) &&
   'Required field'
 
-export const minLength = (min) => (value) => !isEmpty(value) && value.length < min &&
+export const minLength = min => value => !isEmpty(value) && value.length < min &&
   `Must be at least ${min} characters`
 
-export const maxLength = (max) => (value) => !isEmpty(value) && value.length > max &&
+export const maxLength = max => value => !isEmpty(value) && value.length > max &&
   `Must be no more than ${max} characters`
 
-export const integer = (value) => !isInt(value) &&
+export const integer = value => !isInt(value) &&
   'Must be an integer'
 
-export const oneOf = (values) => (value) => !isIn(value, values) &&
+export const oneOf = values => value => !isIn(value, values) &&
   `Must be one of: ${values.join(', ')}`
 
-export const match = (field) => (value, data) => data && value !== data[field] &&
+export const match = field => (value, data) => data && value !== data[field] &&
   'Must match'
 
-export const createValidator = (rules) => (data = {}) => {
+export const createValidator = rules => (data = {}) => {
   const errors = {}
   Object.keys(rules).forEach((key) => {
     const rule = join([].concat(rules[key]))
