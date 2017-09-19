2c87238a50ff15a09e1396519a3d6a8cec72f779
Haz <hazdiego@gmail.com>
2017-09-19 11:55:35 -0300

    Revert "Improved resolution of circularly dependent components (#316)" (#318)
    This reverts commit aef60c9c7c5aa28d599864b00a95be1611f8a1d9.

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,7 @@
-const merge = require('lodash/merge')
-
 // https://github.com/diegohaz/arc/wiki/Atomic-Design#do-not-worry
 const req = require.context('.', true, /\.\/[^/]+\/[^/]+\/index\.js$/)
 
-
-// This exports all components, resolving circular dependencies in two steps:
-// 1. export empty objects in place of each component
-// 2. require each component and merge it into the existing export for that module
-
-const componentPaths = []
-
-// export an empty module for each component
 req.keys().forEach((key) => {
   const componentName = key.replace(/^.+\/([^/]+)\/index\.js/, '$1')
-  componentPaths.push({ componentName, key })
-  module.exports[componentName] = {}
-})
-
-// merge each component into its designated module
-componentPaths.forEach(({ componentName, key }) => {
-  merge(module.exports[componentName], req(key).default)
+  module.exports[componentName] = req(key).default
 })
