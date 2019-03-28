e5f48744485662f23d9fd902868f6a2da013946f
Breno Mazieiro <breno.mazieiro@dotgroup.com.br>
2019-03-28 14:43:11 -0300

    reconfig webpack

--- private/storybook/webpack.config.js
+++ private/storybook/webpack.config.js
@@ -1,14 +1,10 @@
 const baseConfig = require('../../webpack.config')
 
-module.exports = storybookBaseConfig =>
-  Object.assign({}, storybookBaseConfig, {
-    entry: Object.assign({}, storybookBaseConfig.entry, {
-      preview: ['babel-polyfill'].concat(storybookBaseConfig.entry.preview),
-    }),
-    resolve: Object.assign({}, storybookBaseConfig.resolve, {
-      modules: baseConfig.resolve.modules,
-    }),
-    module: Object.assign({}, storybookBaseConfig.module, {
-      rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
-    }),
-  })
+module.exports = storybookBaseConfig => Object.assign({}, storybookBaseConfig, {
+  resolve: Object.assign({}, storybookBaseConfig.resolve, {
+    modules: baseConfig.resolve.modules,
+  }),
+  module: Object.assign({}, storybookBaseConfig.module, {
+    rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
+  }),
+})
