fceb1490ee3ea1cd67d31f2ce05eb857bfd7554c
Breno Mazieiro <breno.mazieiro@localhost.localdomain>
2019-03-28 19:06:47 -0300

    composeWithDevTools as reduxdevtools when configure

--- src-example/store/configure.js
+++ src-example/store/configure.js
@@ -1,13 +1,14 @@
 // https://github.com/diegohaz/arc/wiki/Redux-modules
 import { createStore, applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import createSagaMiddleware from 'redux-saga'
 import { isDev, isBrowser } from 'config'
 import middlewares from './middlewares'
 import reducer from './reducer'
 import sagas from './sagas'
 
-const devtools = isDev && isBrowser && window.window.__REDUX_DEVTOOLS_EXTENSION__
-  ? window.window.__REDUX_DEVTOOLS_EXTENSION__
+const devtools = isDev && isBrowser
+  ? composeWithDevTools({})
   : () => fn => fn
 
 const configureStore = (initialState, services = {}) => {
