f63ca7283a539d242319fcfcef072c4a4b9228ca
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-08-21 21:01:00 -0300

    Updating all packages and storybook

--- private/storybook/webpack.config.js
+++ private/storybook/webpack.config.js
@@ -1,10 +1,13 @@
 const baseConfig = require('../../webpack.config')
 
-module.exports = storybookBaseConfig => Object.assign({}, storybookBaseConfig, {
-  resolve: Object.assign({}, storybookBaseConfig.resolve, {
-    modules: baseConfig.resolve.modules,
-  }),
-  module: Object.assign({}, storybookBaseConfig.module, {
-    rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
-  }),
-})
+module.exports = (wpConfig) => {
+  const storybookBaseConfig = wpConfig.config
+  return Object.assign({}, storybookBaseConfig, {
+    resolve: Object.assign({}, storybookBaseConfig.resolve, {
+      modules: baseConfig.resolve.modules,
+    }),
+    module: Object.assign({}, storybookBaseConfig.module, {
+      rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
+    }),
+  })
+}
