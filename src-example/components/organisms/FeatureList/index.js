c90e60c21bca877965e93142f23bf37c8456a122
Richard Shea <rshea@thecubagroup.com>
2021-02-17 01:03:02 +1300

    Update index.js to fix two spelling mistakes (#380)
    These are minor errors but they are both rather prominent in the page.

--- src-example/components/organisms/FeatureList/index.js
+++ src-example/components/organisms/FeatureList/index.js
@@ -39,9 +39,9 @@ const FeatureList = ({ ...props }) => (
   <div {...props}>
     <StyledHeading>Basic stack</StyledHeading>
     <Description>
-      It includes everything necessary to build a tipical web app with focus on productivity and developer experience.
+      It includes everything necessary to build a typical web app with focus on productivity and developer experience.
       <br />
-      <Link href="https://github.com/diegohaz/arc/wiki/Workflow">Learn more about the recomended workflow</Link>
+      <Link href="https://github.com/diegohaz/arc/wiki/Workflow">Learn more about the recommended workflow</Link>
     </Description>
     <Grid>
       <StyledFeature
